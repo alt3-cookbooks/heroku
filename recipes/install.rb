9c4cfe12977313e7e5265fa2ce4585f005f07ab0
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-10-10 12:58:17 +0200

    Normalizes attributes

--- recipes/install.rb
+++ recipes/install.rb
@@ -6,8 +6,8 @@ include_recipe 'ark::default'
 
 # Create binary heroku and add bin to PATH
 ark "heroku" do
-  url node.heroku[:client_url]
-  path node.heroku[:installation_path]
+  url node[:heroku][:client_url]
+  path node[:heroku][:installation_path]
   creates 'heroku'
   append_env_path true
   action :install
