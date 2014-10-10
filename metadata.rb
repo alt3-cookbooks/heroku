52a934773c0f05fb4334aaad6c8e62a927181086
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-10-10 20:21:53 +0200

    Restructures layout

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,9 @@
 name             'heroku'
+
 maintainer       'alt3.io'
 license          'MIT'
 description      'Installs Heroku using standalone, apt-toolbelt or gem method.'
-version          '0.1'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1'
+
 depends 'ark'
