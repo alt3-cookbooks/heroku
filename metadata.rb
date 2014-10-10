4d7c34b677a3a863bdd572f3c8a738392b4d6929
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-10-10 20:17:14 +0200

    Renames description

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'heroku'
 maintainer       'alt3.io'
 license          'MIT'
-description      'Installs Heroku (standalone or complete toolbelt).'
+description      'Installs Heroku using standalone, apt-toolbelt or gem method.'
 version          '0.1'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 depends 'ark'
