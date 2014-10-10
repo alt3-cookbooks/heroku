595d6601836ecf8b2217fd7c54b6745e97bf9508
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-10-10 12:42:06 +0200

    Includes ark::default

--- recipes/install.rb
+++ recipes/install.rb
@@ -2,11 +2,12 @@
 # Cookbook Name:: heroku
 # Recipe:: default
 #
+include_recipe 'ark::default'
 
 # Create binary heroku and add bin to PATH
 ark "Heroku standalone" do
-  url default[:heroku][:client_url]
-  path default[:heroku][:installation_path]
+  url node.heroku[:client_url]
+  path node.heroku[:installation_path]
   creates 'heroku'
   append_env_path true
   action :install
