9e7c183815a52f2bb46c58c422b36521e181edef
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-10-11 12:19:42 +0200

    Replaces apt source method

--- recipes/toolbelt.rb
+++ recipes/toolbelt.rb
@@ -3,7 +3,7 @@
 # Recipe:: toolbelt
 #
 # Description:
-# Installs the Heroku toollbelt
+# Installs Heroku toolbelt the Chef way using apt as pr
 #
 
 #  echo "deb http://toolbelt.heroku.com/ubuntu ./" > /etc/apt/sources.list.d/heroku.list
@@ -11,10 +11,25 @@
 #  apt-get update
 #  apt-get install -y heroku-toolbelt
 
-# Add heroku source list to apt
-template node[:heroku][:toolbelt][:source_list] do
-  source 'heroku.list.erb'
-  notifies :run, 'execute[apt-get update]', :immediately
-  not_if { ::File.exists?(node[:heroku][:toolbelt][:source_list]) }
+
+apt_repository 'heroku' do
+  uri        'http://toolbelt.heroku.com/ubuntu'
+  components ['main', 'stable']
+  key        'https://toolbelt.heroku.com/apt/release.key'
 end
 
+
+
+
+# Add heroku source list to apt
+#template node[:heroku][:toolbelt][:source_list] do
+#  source 'heroku.list.erb'
+#  notifies :run, 'execute[apt-get update]', :immediately
+#  not_if { ::File.exists?(node[:heroku][:toolbelt][:source_list]) }
+#end
+
+#remote_file "#{Chef::Config[:file_cache_path]}/distribute_setup.py" do
+#  source "http://python-distribute.org/distribute_setup.py"
+#  mode '0644'
+#  not_if { ::File.exists?(pip_binary) }
+#end
