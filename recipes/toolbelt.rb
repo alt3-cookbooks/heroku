13d1d7e1f7fd3c098a732ea61274c9f5fd2a2cec
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-10-11 12:03:46 +0200

    Adds heroku to apt source list

--- recipes/toolbelt.rb
+++ recipes/toolbelt.rb
@@ -2,3 +2,18 @@
 # Cookbook Name:: heroku
 # Recipe:: toolbelt
 #
+# Description:
+# Installs the Heroku toollbelt
+#
+
+#  echo "deb http://toolbelt.heroku.com/ubuntu ./" > /etc/apt/sources.list.d/heroku.list
+#  wget -O- https://toolbelt.heroku.com/apt/release.key | apt-key add -
+#  apt-get update
+#  apt-get install -y heroku-toolbelt
+
+# Add heroku source list to apt
+template '/etc/apt-cacher-ng/acng.conf' do
+  source 'heroku.list.erb'
+  notifies :run, 'execute[apt-get update]', :immediately
+end
+
