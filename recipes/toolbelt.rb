ab5ce5d3e66b3ee1d4af9af802f6dac30c2003d4
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-10-11 12:35:37 +0200

    Changes source to heroku's ./

--- recipes/toolbelt.rb
+++ recipes/toolbelt.rb
@@ -2,32 +2,21 @@
 # Cookbook Name:: heroku
 # Recipe:: toolbelt
 #
-# Description:
-# Chef translated installation of Heroku's install-ubuntu.sh:
+# Literal Chef translation of Heroku's install-ubuntu.sh script:
 #      echo "deb http://toolbelt.heroku.com/ubuntu ./" > /etc/apt/sources.list.d/heroku.list
 #      wget -O- https://toolbelt.heroku.com/apt/release.key | apt-key add -
 #      apt-get update
 #      apt-get install -y heroku-toolbelt
+#
 
-
+# Add heroku to apt sources
 apt_repository 'heroku' do
   uri        'http://toolbelt.heroku.com/ubuntu'
-  components ['stable']
+  components ['./']
   key        'https://toolbelt.heroku.com/apt/release.key'
 end
 
-
-
-
-# Add heroku source list to apt
-#template node[:heroku][:toolbelt][:source_list] do
-#  source 'heroku.list.erb'
-#  notifies :run, 'execute[apt-get update]', :immediately
-#  not_if { ::File.exists?(node[:heroku][:toolbelt][:source_list]) }
-#end
-
-#remote_file "#{Chef::Config[:file_cache_path]}/distribute_setup.py" do
-#  source "http://python-distribute.org/distribute_setup.py"
-#  mode '0644'
-#  not_if { ::File.exists?(pip_binary) }
+# Install heroku-toolbelt packages
+#package 'heroku-toolbelt' do
+#  action :install
 #end
