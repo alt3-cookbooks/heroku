435195328fc2da360aa9abd1e3c2d0ab6f65fe07
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-10-11 14:38:33 +0200

    Bumps version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer       'alt3.io'
 license          'MIT'
 description      'Installs Heroku using standalone, toolbelt or gem method.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1'
+version          '1.0.0'
 
 depends 'ark'
